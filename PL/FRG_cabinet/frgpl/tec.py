f522a15298122571a8712f3a5617a8f78673708b
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2019-09-10 15:04:49 -0700

    working code to build commands to TEC

--- PL/FRG_cabinet/frgpl/tec.py
+++ PL/FRG_cabinet/frgpl/tec.py
@@ -11,14 +11,15 @@ class tec:
 
 	def connect(self, port = 'COM12', address = 1):
 		self.__handle = serial.Serial()
+		self.__handle.port = port
 		self.__handle.timeout = 2
 		self.__handle.parity = 'E'
 		self.__handle.bytesize = 7
-		self.__handle.baudrate = 19200
+		self.__handle.baudrate = 9600
 		self.__handle.open()
 		
 		#configure communication bits
-		self.__address = numtohex(address)	#convert to hex, for use in communication <addr>
+		self.__address = self.numtohex(address)	#convert to hex, for use in communication <addr>
 		self.__end = b'\r\n'	#end bit <etx>
 
 		return True
@@ -29,22 +30,27 @@ class tec:
 
 
 	### helper methods
-	def numtohex(num):
-		return codecs.encode(str.encode('{0:02d}'.format(num)), 'hex_codec')
+	def numtohex(self, num):
+		# return codecs.encode(str.encode('{0:02d}'.format(num)), 'hex_codec')
+		return '{0:02X}'.format(num).encode()
 
 	def buildPayload(self, command, dataAddress, numWords):
+		def calculateChecksum(payload):
+			numHexValues = int(len(payload)/2)
+			hexValues = [int(payload[2*i : (2*i)+2], 16) for i in range(numHexValues)]
+			checksum = hex(256 - sum(hexValues))[-2:]	#drop the 0x convention at front, we only want the last two characters
+
+			return str.upper(checksum).encode()
+
 		payload = self.__address
-		payload = payload + numtohex(command)
+		payload = payload + self.numtohex(command)
 		payload = payload + str.encode(str(dataAddress))
 		payload = payload + str.encode(str('{0:04d}'.format(numWords)))
 		
 		# calculate checksum from current payload
-		chksum = sum([int(payload[2*i : (2*i)+2], 16) for i in range(int(len(payload)/2))]) 	#add all bits as decimal values
-		chksum = hex(chksum % 256) #convert back to hex, only keep two bits
-		chksum = codecs.encode(str.encode(chksum[-2:], 'hex_codec'))	#only consider the last bit for checksum
-
+		chksum =calculateChecksum(payload)
+		
 		# complete the payload
-
 		payload = payload + chksum
 		payload = payload + self.__end
 		payload = b':' + payload  	#should start with ":", just held til the end to not interfere with checksum calculation
