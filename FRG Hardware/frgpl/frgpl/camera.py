87cd4d59307054ff0758373c0641e439b9b54932
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2020-03-05 16:08:14 -0800

    add hayear object

--- FRG Hardware/frgpl/frgpl/camera.py
+++ FRG Hardware/frgpl/frgpl/camera.py
@@ -9,6 +9,8 @@ import matplotlib.animation as animation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import time
 from scipy.signal import medfilt
+import cv2
+from functools import partial
 
 class camera:
 	def __init__(self, port = None):
@@ -136,4 +138,64 @@ class camera:
 		plt.ion()
 		plt.show()
 		input('Press [enter] to continue. Close preview window to release camera control.')
+
+class hayear:
+	def __init__(self, id = 0)
+		self._resolution = [1080, 1920]
+		self.connect()
+	
+	def connect(self, address = 0):
+		self.cap = cv2.VideoCapture(0) #assumes first camera is the target camera
+		self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self._resolution[1])
+		self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self._resolution[0])	
+	
+		return True
+
+	def disconnect(self):
+		try:
+			self.cap.release()
+		except:
+			pass
+
+		return True
+
+	def capture(self, numframes = 10, imputeHotPixels = False):
+		raw = np.zeros((numframes, self._resolution[0], self._resolution[1], 3))
+
+		for idx in range(numframes):
+			ret, currentframe = self.cap.read()
+			raw[idx] = cv2.cvtColor(currentframe, cv2.COLOR_BGR2RGB)/255 #cv2 reads RGB channels as BGR, this puts it back to RGB
+
+		avg = np.mean(raw, axis = 0)
+		std = np.std(raw, axis = 0)
+
+		if imputeHotPixels:
+			mask = avg > (avg.mean() + 3*avg.std())	#flag values 3 std devs over the mean
+			medvals = medfilt(avg, 3)	#3x3 median filter
+			avg[mask] = medvals[mask]
+
+		return avg, std, raw
+
+	def preview(self):
+		def animate(i, im):
+			ax.clear()
+			_, _, img = self.capture()
+			im.set_data(img)
+			# cb.set_clim((img.min(), img.max()))
+
+		def handle_close(evt, cam = self._cam):
+			plt.ioff()
+
+		plt.ioff()
+		fig, ax = plt.subplots()
+		_, _, img = self.capture()
+		im = ax.imshow(img)
+		#divider = make_axes_locatable(ax)
+		#cax = divider.append_axes('right', size='5%', pad=0.05)
+		ani = animation.FuncAnimation(fig, partial(animate, im = im), interval=100, blit = True) 
+		fig.canvas.mpl_connect('close_event', lambda x: handle_close(x, cam = self._cam))
+
+		plt.ion()
+		plt.show()
+		input('Press [enter] to continue.')
 
