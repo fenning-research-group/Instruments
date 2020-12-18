a0b22f2c5fb022eb374072330cb565bcd9a7b499
rishi <rek010@eng.ucsd.edu>
2020-12-18 11:10:01 -0800

    take all measurements in buffer, parse after sweep

--- FRG Hardware/frghardware/keithleyjv/frghardware/keithleyjv/control.py
+++ FRG Hardware/frghardware/keithleyjv/frghardware/keithleyjv/control.py
@@ -18,6 +18,7 @@ class Control:
 		self.keithley = Keithley2400(keithley_address)
 		self.keithley.reset()
 		# self.keithley.output_off_state = 'HIMP'
+		self.keithley.use_front_terminals()
 		self.keithley.wires = 4
 		self.keithley.apply_voltage()
 		
@@ -39,12 +40,40 @@ class Control:
 		# self.shutter.write(b'0')
 		# self._shutteropen = False
 		return
-		
-	def jsc(self):
+
+	def _source_voltage_measure_current():
 		self.keithley.apply_voltage()
 		self.keithley.measure_current()
 		self.keithley.compliance_current = 1.05
 		self.keithley.souce_voltage = 0
+
+	def _source_current_measure_voltage():
+		self.keithley.apply_current()
+		self.keithley.measure_voltage()
+		self.keithley.compliance_voltage = 2
+		self.keithley.souce_current = 0
+
+	def _set_buffer(self, npts):
+		self.keithley.disable_buffer()
+		self.keithley.config_buffer(self.counts, 0)
+		self.keithley.buffer_points = self.counts * npts
+		self.keithley.reset_buffer()
+
+	def _parse_buffer(self, npts):
+		alldata = self.buffer_data
+		means = np.zeros((npts,))
+		stds = np.zeros((npts,))
+		for i in range(npts):
+			idx = slice(i*npts, (i+1)*npts)
+			means[i] = np.mean(alldata[idx])
+			stds[i] = np.std(alldata[idx])
+		return {
+			'mean':means,
+			'std':stds
+		}
+
+	def jsc(self):
+		self._source_voltage_measure_current()
 		self.keithley.enable_source()
 		self.open_shutter()
 
@@ -57,10 +86,7 @@ class Control:
 		return jsc
 
 	def voc(self):
-		self.keithley.apply_current()
-		self.keithley.measure_voltage()
-		self.keithley.compliance_voltage = 2
-		self.keithley.souce_current = 0
+		self._source_current_measure_voltage()
 		self.keithley.enable_source()
 		self.open_shutter()
 
@@ -73,27 +99,22 @@ class Control:
 		return voc
 
 	def jv(self, name, vmin = -0.2, vmax = 0.7, steps = 50):
-		self.keithley.apply_voltage()
-		self.keithley.compliance_current = 1.05
+		self._source_voltage_measure_current()
 		self.keithley.source_voltage = vmin
-		self.keithley.config_buffer(50, 0) #50 pts, 0 delay between
+		self._set_buffer(npts = steps)
 		v = np.linspace(vmin, vmax, steps)
-		i = np.zeros(v.shape)
 
 		self.keithley.enable_source()
 		self.open_shutter()
 		for m, v_ in enumerate(v):
 			self.keithley.source_voltage = v_
-			self.keithley.disable_buffer()
-			self.keithley.reset_buffer()
 			time.sleep(self.pause)
 			self.keithley.start_buffer()
-			self.keithley.wait_for_buffer()
-			i[m] = self.keithley.mean_current
-			# i_std[m] = self.keithley.standard_devs
+			# self.keithley.wait_for_buffer()
 		self.close_shutter()
 		self.keithley.disable_source()
 
+		i = -self._parse_buffer(npts = steps)['mean']
 		j = i*1000/self.area #amps to mA/cm2
 
 		data = pd.DataFrame({
@@ -104,31 +125,27 @@ class Control:
 		data.to_csv(f'{name}_light.csv')
 
 	def darkjv(self, name, vmin = -0.2, vmax = 0.7, steps = 50):
-		self.keithley.apply_voltage()
-		self.keithley.compliance_current = 1.05
+		self._source_voltage_measure_current()
 		self.keithley.source_voltage = vmin
-
+		self._set_buffer(npts = steps)
 		v = np.linspace(vmin, vmax, steps)
-		i = np.zeros(v.shape)
 
 		self.keithley.enable_source()
 		self.close_shutter()
 		for m, v_ in enumerate(v):
 			self.keithley.source_voltage = v_
-			self.keithley.reset_buffer()
 			time.sleep(self.pause)
 			self.keithley.start_buffer()
-			self.keithley.wait_for_buffer()
-			i[m] = self.keithley.mean_current
-			# i_std[m] = self.keithley.standard_devs
 		self.keithley.disable_source()
 
-		j = i*1000/self.area	#amps to mA/cm2
-
+		i = self._parse_buffer(npts = steps)['mean']
+		j = i*1000/self.area #amps to mA/cm2
+		logj = np.log(np.abs(j))
 		data = pd.DataFrame({
 		    'Voltage (V)': voltages,
 		    'Current Density (mA/cm2)': j,
 		    'Current (A)': i,
+		    'Log Current Density': logj
 		})
 		data.to_csv(f'{name}_dark.csv')
 
