b9e258d7dd5d90722fae42e83c93de78865c0ced
rishi-e-kumar <47284667+rishi-e-kumar@users.noreply.github.com>
2020-12-21 09:47:45 -0800

    modifications

--- FRG Hardware/frghardware/keithleyjv/frghardware/keithleyjv/control.py
+++ FRG Hardware/frghardware/keithleyjv/frghardware/keithleyjv/control.py
@@ -41,13 +41,13 @@ class Control:
 		# self._shutteropen = False
 		return
 
-	def _source_voltage_measure_current():
+	def _source_voltage_measure_current(self):
 		self.keithley.apply_voltage()
 		self.keithley.measure_current()
 		self.keithley.compliance_current = 1.05
 		self.keithley.souce_voltage = 0
 
-	def _source_current_measure_voltage():
+	def _source_current_measure_voltage(self):
 		self.keithley.apply_current()
 		self.keithley.measure_voltage()
 		self.keithley.compliance_voltage = 2
@@ -55,12 +55,11 @@ class Control:
 
 	def _set_buffer(self, npts):
 		self.keithley.disable_buffer()
-		self.keithley.config_buffer(self.counts, 0)
-		self.keithley.buffer_points = self.counts * npts
+		self.keithley.buffer_points = self.counts
 		self.keithley.reset_buffer()
 
 	def _parse_buffer(self, npts):
-		alldata = self.buffer_data
+		alldata = self.keithley.buffer_data
 		means = np.zeros((npts,))
 		stds = np.zeros((npts,))
 		for i in range(npts):
@@ -71,56 +70,65 @@ class Control:
 			'mean':means,
 			'std':stds
 		}
+	def measure(self):
+		"""
+		returns voltage, current, and resistance measured
+		"""
+		self.keithley.config_buffer(self.counts)
+		self.keithley.start_buffer()
+		self.keithley.wait_for_buffer()
+		return self.keithley.means
 
 	def jsc(self):
 		self._source_voltage_measure_current()
+		self.source_voltage = 0
 		self.keithley.enable_source()
 		self.open_shutter()
 
-		isc = -self.keithley.current
-
+		isc = -self.measure()[1]
+		jsc = isc*1000/self.area
 		self.close_shutter()
 		self.keithley.disable_source()
-		print(f'Isc: {isc} A, Jsc: {jsc*1000/self.area}')
+		print(f'Isc: {isc:.3f} A, Jsc: {jsc:.2f} mA/cm2')
 
 		return jsc
 
 	def voc(self):
 		self._source_current_measure_voltage()
+		self.souce_current = 0
 		self.keithley.enable_source()
 		self.open_shutter()
 
-		voc = self.keithley.voltage
+		voc = self.measure()[0]
 
 		self.close_shutter()
 		self.keithley.disable_source()
-		print(f'Voc: {voc} V')
+		print(f'Voc: {voc*1000:.2f} mV')
 
 		return voc
 
 	def jv(self, name, vmin = -0.2, vmax = 0.7, steps = 50):
 		self._source_voltage_measure_current()
 		self.keithley.source_voltage = vmin
-		self._set_buffer(npts = steps)
+		# self._set_buffer(npts = steps)
 		v = np.linspace(vmin, vmax, steps)
+		vmeas = np.zeros((steps,))
+		i = np.zeros((steps,))
 
 		self.keithley.enable_source()
 		self.open_shutter()
 		for m, v_ in enumerate(v):
 			self.keithley.source_voltage = v_
-			time.sleep(self.pause)
-			self.keithley.start_buffer()
-			# self.keithley.wait_for_buffer()
+			vmeas[m], i[m], _ = self.measure()
 		self.close_shutter()
 		self.keithley.disable_source()
-
-		i = -self._parse_buffer(npts = steps)['mean']
 		j = i*1000/self.area #amps to mA/cm2
 
 		data = pd.DataFrame({
-		    'Voltage (V)': voltages,
+		    'Voltage (V)': v,
 		    'Current Density (mA/cm2)': j,
 		    'Current (A)': i,
+		    'Measured Voltage (V)': vmeas
 		})
 		data.to_csv(f'{name}_light.csv')
 
