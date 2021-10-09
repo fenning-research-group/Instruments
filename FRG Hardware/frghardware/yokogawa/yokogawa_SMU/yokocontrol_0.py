5042015e59c13e7767fb655f111c8b981d6aeb1f
Deniz93 <49666626+Deniz93@users.noreply.github.com>
2021-10-09 13:43:05 -0700

    Update yokocontrol_0.py

--- FRG Hardware/frghardware/yokogawa/yokogawa_SMU/yokocontrol_0.py
+++ FRG Hardware/frghardware/yokogawa/yokogawa_SMU/yokocontrol_0.py
@@ -190,7 +190,7 @@ class Control:
 
 	def tseries_jv(self, name, vmin=-0.1, vmax=1, steps=500, area = 3, reverse = True, forward = True, preview=True, totaltime=3600, breaktime=60):
 		
-		steps = np.round(totaltime  / breaktime,1)
+		steps = int(np.round(totaltime  / breaktime,1))
 
 
 		for n in range(steps):
